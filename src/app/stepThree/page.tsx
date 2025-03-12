df1ea62b00c35227369483cd24c4469191586e5c
cdnbelgium <jscharlach@hotmail.com>
2025-03-12 11:08:25 +0100

    updated logic for a user and full details about EMR and devices

--- src/app/stepThree/page.tsx
+++ src/app/stepThree/page.tsx
@@ -19,6 +19,11 @@ const StepThree = () => {
     }
   }, [isAuthenticated, router]);
 
+
+  if (!user) {
+    return null; // or a loading indicator
+  }
+  
   console.log("profile page", user);
 
   // Pricing Calculation Function
@@ -30,12 +35,13 @@ const StepThree = () => {
     return basePrice + additionalDoctors + additionalDevices;
 }
 
+
   const totalDoctors = user.doctors.length;
   const totalDevices = user.selectedDevices.length;
   const monthlyPrice = calculatePricing(totalDoctors, totalDevices);
 
   const handleActivateClick = () => {
-    const totalAmount = monthlyPrice * 100; 
+    const totalAmount = monthlyPrice * 100; // Convert to cents for Stripe
 
     router.push(
       `/checkout?packageName=${encodeURIComponent(user.practice)}&amount=${totalAmount}&devices=${totalDevices}&price=${monthlyPrice}`
@@ -44,24 +50,24 @@ const StepThree = () => {
 
   return (
     <>
-      <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28 mt-8">
+      <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28 mt-14">
         <SectionTitle
           title="Simple and Affordable Pricing"
           paragraph="With the Configuration selected."
           center
           width="665px"
         />
-        <div className="relative z-10 w-1/2 mx-auto rounded-sm bg-white px-8 py-10 shadow-three hover:shadow-one dark:bg-gray-dark dark:shadow-two dark:hover:shadow-gray-dark">
+        <div className="relative z-10 w-full sm:w-3/4 md:w-2/3 lg:w-1/2 mx-auto rounded-sm bg-white px-8 py-10 shadow-three hover:shadow-one dark:bg-gray-dark dark:shadow-two dark:hover:shadow-gray-dark">
           <div className="mx-auto">
             <div>
-              <p className="text-5xl font-bold dark:text-white mb-10 text-center">
+              <p className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl font-bold dark:text-white mb-6 text-center">
                 Practice Name: <span className="ml-2">{user.practice}</span>
               </p>
               <hr className="w-3/5 mx-auto border-t border-primary mt-4" />
 
               {/* Display Doctors */}
               <div className="mt-4">
-                <h3 className="text-xl font-semibold dark:text-white mb-2">Additional Doctors:</h3>
+                <h3 className="text-xl font-semibold dark:text-white mb-2">Total Doctors:</h3>
                 <ul className="list-disc list-inside">
                   {user.doctors.map((doctor, index) => (
                     <li key={doctor.drsId || index} className="text-lg">
@@ -105,22 +111,21 @@ const StepThree = () => {
               <div className="mt-4 text-center">
                 <h3 className="text-2xl font-semibold dark:text-white">Monthly Subscription Cost</h3>
                 <p className="text-3xl font-bold text-primary mt-2">€{monthlyPrice}/month</p>
+                <p className="text-sm font-bold text-primary mt-2">VAT included</p>
               </div>
             </div>
             <div className="mb-8 mt-6 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
-              <button
+            <button
                 onClick={handleActivateClick}
-                className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+                disabled={!user.admin} // Disable if user.admin is false
+                className={`flex w-full items-center justify-center p-3 text-base font-semibold text-white transition duration-300 ease-in-out 
+                  ${user.admin ? "bg-primary hover:bg-opacity-80 hover:shadow-signUp" : "bg-gray-400 cursor-not-allowed"}`}
               >
                 Activate
-              </button>
+            </button>
             </div>
           </div>
         </div>
-
-        {/* <div className="flex justify-center mt-6">
-          <Pricing />
-        </div> */}
         <CircleBackground />
       </section>
     </>
